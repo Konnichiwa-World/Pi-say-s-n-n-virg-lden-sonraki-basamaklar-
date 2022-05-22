203819b9f6c27ee9440e0c6608146452eb58e28c
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-05-22 22:19:32 +0300

    Pi sayısının virgülden sonrası
    Pi sayısının virgülden sonraki, istenilen basamağındaki rakamı bulma programı.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+class program
+{
+    public static void Main(string[] args)
+    { a1:
+        System.Console.WriteLine("Pi sayısının, virgülden sonra kaçıncı basamağında ne var?\nLütfen basamak giriniz:");
+        Int32 x =Int32.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(Convert.ToInt32((Math.PI*(Math.Pow(10,x)))%10));goto a1;
+    }
+}
